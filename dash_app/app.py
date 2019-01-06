bf8dd8926a89efa4aa6737551443e0ecf1626d0a
agahkarakuzu <agahkarakuzu@gmail.com>
2019-01-06 01:39:07 -0500

    Formatting, file loading etc.

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,56 +1,64 @@
-# Data driven callbacks 
-# Instead of this you can have a slider and chart interact with each other as well.as
-
-import dash
-from dash.dependencies import Input, Output
-import dash_html_components as html
-import dash_core_components as dcc
-from flask import Flask
-import pandas as pd
-
 import base64
 import os
 from urllib.parse import quote as urlquote
 
+from flask import Flask, send_from_directory
+import dash
 import dash_core_components as dcc
+import dash_html_components as html
 from dash.dependencies import Input, Output
 
-UPLOAD_DIRECTORY = "/Users/Agah/Desktop/DOcean/dash_app/data"
+UPLOAD_DIRECTORY = "/Users/Agah/Desktop/KuzuHub/svds.dash/dash_app/data"
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
 server = Flask(__name__)
+app = dash.Dash(server=server)
 
+@server.route("/download/<path:path>")
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
-app = dash.Dash(name='agah',server=server,csrf_protect=False)
+app.config['suppress_callback_exceptions']=True
 
 app.layout = html.Div([
-
+    html.Div(
+    className = "footer",
+    children = [
         dcc.Upload(
             id="upload-data",
             children=html.Div(
-                [" "]
+                ["Drag and drop or click to select SVDS files."]
             ),
             style={
-                "width": "300px",
-                "color": "#fff",
-                "height": "300px",
-                "lineHeight": "60px",
-                "borderWidth": "4px",
-                "borderStyle": "none",
-                "borderRadius": "50%",
-                "margin": "10% 20% 0% 40%",
+                "width": "100%",
+                "height": "80px",
+                "lineHeight": "200px",
+                "textIndent": "10px",
                 "textAlign": "center",
-                "background": "#fbdeda",
-                
+                "margin": "10px",
+                'background-image': 'url(https://github.com/agahkarakuzu/svds/blob/master/svds2.png?raw=true)',
+                'background-size': 'contain',
+                'background-repeat': 'no-repeat',
+                'background-position': 'center'
             },
-            multiple=False,
+            multiple=True,
         ),
-        html.H2(" "),
-        html.Ul(id="file-list")
-
-])
+    ],
+    style={"max-width": "500px"}),
+    html.Div(
+    className = "row",
+    children = [
+    html.Div(className = "six columns",
+    style={"fontColor":"blue","marginTop":"5%","marginLeft":"15px"},
+    children = [
+    html.H3("File List"),
+    dcc.Checklist(id = "check-me",options =[],values=[])
+    ])])
+    ],
+)
 
 
 
@@ -59,24 +67,23 @@ def save_file(name, content):
     data = content.encode("utf8").split(b";base64,")[1]
     with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
         fp.write(base64.decodebytes(data))
-        
+
 def uploaded_files():
     """List the files in the upload directory."""
     files = []
     for filename in os.listdir(UPLOAD_DIRECTORY):
         path = os.path.join(UPLOAD_DIRECTORY, filename)
-        if os.path.isfile(path):
+        if os.path.isfile(path) and filename != '.DS_Store':
             files.append(filename)
     return files
-    
+
 def file_download_link(filename):
     """Create a Plotly Dash 'A' element that downloads a file from the app."""
     location = "/download/{}".format(urlquote(filename))
     return html.A(filename, href=location)
 
-
 @app.callback(
-     Output("file-list", "children"),
+    Output("check-me", "options"),
     [Input("upload-data", "filename"), Input("upload-data", "contents")],
 )
 def update_output(uploaded_filenames, uploaded_file_contents):
@@ -88,13 +95,11 @@ def update_output(uploaded_filenames, uploaded_file_contents):
 
     files = uploaded_files()
     if len(files) == 0:
-        return [html.Li("No files yet!")]
+        return {'label':'No files yet!','value':"Empty"}
     else:
-        return [html.Li(file_download_link(filename)) for filename in files]
+        return [{'label':filename,'value':filename} for filename in files]
 
 
 
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8888)
